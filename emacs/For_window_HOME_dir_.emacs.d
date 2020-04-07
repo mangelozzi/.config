45669fecfbd276674238461833b18af8cdd3ba78
Michael Angelozzi <mangelozzi@gmail.com>
2020-04-07 23:53:01 +0200

    File not helpfful

--- emacs/For_window_HOME_dir_.emacs.d
+++
@@ -1,6 +0,0 @@
-;; Place this file in C:/Users/Username/AppData/Roaming and point to the appropriate files
-(setq user-init-file "C:/Users/Michael/.config/emacs/init.el")
-(setq user-emacs-directory C:/Users/Michael/.config/emacs)
-;; (setq default-directory "C:/whatever/you/want/to/start/in")
-;; (setenv "HOME" "D:/my/home/directory")
-(load user-init-file)
