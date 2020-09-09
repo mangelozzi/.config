c60b7e4b149f8f7b3573c65b263c0f4c51efd416
Michael Angelozzi <mangelozzi@gmail.com>
2020-09-09 12:58:32 +0200

    FZF can only use after re logging into

--- install/2_utils.sh
+++ install/2_utils.sh
@@ -22,9 +22,13 @@ sudo apt install fd-find
 # sudo apt install fzf
 if ! command -v fzf &> /dev/null
 then
-    read -p "FZF will be installed only for the current user, press <ENTER> to continue..."
+    echo "\nFZF will be installed only for the current user."
+    read -p "Press <ENTER> to continue..."
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
+    bash ~/.fzf/install
+    echo
+    echo -e "\nIn order to be able to use FZF you will have to log out and into bash again."
+    read -p "Press <ENTER> to continue..."
 fi
 
 # Tree
